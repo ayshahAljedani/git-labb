7e9168591775af1d806b5c672c12b2c91ea6bc11
Ayshah ALjedani <aa621016@ohio.edu>
2019-05-18 23:25:15 -0400

    change

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ayshah Aljedani
+ *        Date: 18.05.19
+ * Description: print Hello Git!.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << " Hello Git!"<< endl;
+    return 0;
+}// main
